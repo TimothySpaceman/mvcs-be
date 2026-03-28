d87384a2dd7de221e72e18b817630fd742942ef8
Timothy Rehenel <timothy.rehenel@gmail.com>
2026-03-28 21:23:06 +0200

    feat: added api container

---
+++ be/Api/Dummy/DummyController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Dummy;
+
+[ApiController]
+[Route("api/[controller]")]
+public class DummyController : ControllerBase
+{
+    private readonly IConfiguration _config;
+
+    public DummyController(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    [HttpGet]
+    public IActionResult GetHello()
+    {
+        return Ok("Hello World!");
+    }
+    
+    [HttpGet("connections")]
+    public IActionResult GetConnectionStrings()
+    {
+        var section = _config.GetSection("ConnectionStrings");
+
+        var result = section.GetChildren()
+            .ToDictionary(x => x.Key, x => x.Value);
+
+        return Ok(result);
+    }
+}
